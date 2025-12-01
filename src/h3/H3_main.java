73a451aea7b31f677d1a9a04e6847e0240942d77
bkarimig <babaklarimi4.0@gmail.com>
2025-12-01 11:53:50 +0100

    Update H3_main.java

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,5 +1,24 @@
 package h3;
 
 public class H3_main {
+	public static boolean compareArraysVal(int[] a, int[] b) {
+	    if (a == null || b == null) return false;
+	    if (a.length != b.length) return false;
 
+
+	    int[] aCopy = a.clone();
+	    int[] bCopy = b.clone();
+
+	    
+	    java.util.Arrays.sort(aCopy);
+	    java.util.Arrays.sort(bCopy);
+
+	   
+	    for (int i = 0; i < aCopy.length; i++) {
+	        if (aCopy[i] != bCopy[i]) return false;
+	    }
+
+	    return true;
+	}
 }
+
