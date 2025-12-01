dea69568f60686b01cf599d2dfdeccc8129c1152
bkarimig <babaklarimi4.0@gmail.com>
2025-12-01 11:53:10 +0100

    Update H1_Main.java

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -1,5 +1,15 @@
 package h1;
 
 public class H1_Main {
+	public static boolean isMirrorArray(int[] a, int[] b) {
+	    if (a == null || b == null) return false;
+	    if (a.length != b.length) return false;
 
+	    for (int i = 0; i < a.length; i++) {
+	        if (a[i] != b[b.length - 1 - i]) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 }
